49a9c3d62487b79121b6cca7a6a6f285488639f4
Dhanish Buhari <72680307+Dhanish-Buhari@users.noreply.github.com>
2023-10-22 07:16:27 +0530

    Time: 2 ms (53.73%) | Memory: 40.7 MB (30.24%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,29 +1,37 @@
-public class Solution {
-    public boolean backspaceCompare(String S, String T) {
-        char[] sChars = S.toCharArray();
-        char[] tChars = T.toCharArray();
-        
-        int k = processString(sChars);
-        int p = processString(tChars);
-
-        if (k != p) return false;
-
-        for (int i = 0; i < k; i++) {
-            if (sChars[i] != tChars[i]) return false;
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        Stack <Character> stk1 = new Stack<Character>();
+        for(int i=0; i<s.length(); i++){
+            if(stk1.size()>0 && s.charAt(i)=='#'){
+                stk1.pop();
+                continue;
+            }
+                else if(s.charAt(i)=='#'){
+                continue;
+            }
+            stk1.add(s.charAt(i));
         }
-
-        return true;
-    }
-
-    private int processString(char[] chars) {
-        int k = 0;
-        for (char c : chars) {
-            if (c != '#') {
-                chars[k++] = c;
-            } else if (k > 0) {
-                k--;
+        Stack <Character> stk2 = new Stack<>();
+        for(int i=0; i<t.length(); i++){
+            if(stk2.size()>0 && t.charAt(i)=='#'){
+                stk2.pop();
+                continue;
+            }
+            else if(t.charAt(i)=='#'){
+                continue;
+            }
+            stk2.add(t.charAt(i));
+        }
+        //System.out.print(stk1+" "+stk2);
+        while(!stk1.isEmpty() && !stk2.isEmpty()){
+            if(stk1.pop()!=stk2.pop()){
+                return false;
             }
         }
-        return k;
+        if(stk1.size()!=stk2.size()){
+            return false;
+
+        }
+        return true;
     }
 }
