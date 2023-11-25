85fc27cbf85ff5bac3b71daadbb21446a5e5cf7b
Dhanish Buhari <72680307+Dhanish-Buhari@users.noreply.github.com>
2023-11-25 21:38:50 +0530

    Time: 46 ms (100.00%) | Memory: 55 MB (100.00%) - LeetSync

---
+++ 3209-minimum-number-of-coins-for-fruits/minimum-number-of-coins-for-fruits.java
@@ -0,0 +1,30 @@
+class Solution {
+    private int[][] memo;
+    public int minimumCoins(int[] prices) {
+        memo = new int[prices.length][prices.length+1];
+        for (int[] row : memo) Arrays.fill(row, -1);
+        return dp(prices, 0, 0);
+    }
+    
+    public int dp(int[] prices, int i, int freeCount) {
+        if (i >= prices.length) {
+            return 0;
+        }
+        
+        if (memo[i][freeCount] != -1) {
+            return memo[i][freeCount];
+        }
+        
+        // Take and add to freeCount and we did not take the last freeCount
+        int take = dp(prices, i+1, i+1) + prices[i];
+        int takeForFree = Integer.MAX_VALUE;
+        
+        if (freeCount > 0) {
+            takeForFree = dp(prices, i+1, freeCount-1);
+        }
+        
+        
+        return memo[i][freeCount] = Math.min(take, takeForFree);
+        
+    }
+}
