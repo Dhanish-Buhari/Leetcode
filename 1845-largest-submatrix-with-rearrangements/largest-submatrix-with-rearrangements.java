1c11ce5b8c9050a59a37ecd84ef5b033fb84f873
Dhanish Buhari <72680307+Dhanish-Buhari@users.noreply.github.com>
2023-11-26 19:22:07 +0530

    Time: 9 ms (96.39%) | Memory: 67.6 MB (46.99%) - LeetSync

--- 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.java
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.java
@@ -1,21 +1,25 @@
 class Solution {
     public int largestSubmatrix(int[][] matrix) {
-        int m = matrix.length, n = matrix[0].length;
-        for (int i = 1; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        for (int i = 1; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 if (matrix[i][j] == 1) {
-                    matrix[i][j] = matrix[i - 1][j] + 1;
+                    matrix[i][j] += matrix[i - 1][j];
                 }
             }
         }
-        int ans = 0;
-        for (var row : matrix) {
-            Arrays.sort(row);
-            for (int j = n - 1, k = 1; j >= 0 && row[j] > 0; --j, ++k) {
-                int s = row[j] * k;
-                ans = Math.max(ans, s);
+
+        int res = 0;
+        for (int i = 0; i < row; i++) {
+            Arrays.sort(matrix[i]);
+            for (int j = 0; j < col; j++) {
+                int height = matrix[i][j];
+                int width = col - j;
+                res = Math.max(res, height * width);
             }
         }
-        return ans;
+
+        return res;        
     }
 }
